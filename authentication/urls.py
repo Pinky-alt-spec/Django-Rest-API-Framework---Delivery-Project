d4702008e9119c803cfd04044fdbb0527a866ef7
Pinky Mononyane <pinkymononyane@gmail.com>
2023-10-03 14:27:02 +0200

    serialize data

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.AuthView.as_view(), name="Hello world")
+    path('', views.AuthView.as_view(), name="Hello world"),
+    path('signup/', views.UserCreateView.as_view(), name="sign_up")
 ]
