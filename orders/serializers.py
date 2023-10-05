d4481f58fb9a7a7123efe0415e2987e50dd558db
Pinky Mononyane <pinkymononyane@gmail.com>
2023-10-05 14:13:50 +0200

    test endpoints

--- orders/serializers.py
+++ orders/serializers.py
@@ -11,6 +11,19 @@ class OrderCreationSerializer(serializers.ModelSerializer):
         fields=['id', 'size', 'order_status', 'quantity']
         
         
+class OrderDetailSerializer(serializers.ModelSerializer):
+    size=serializers.CharField(max_length=20)
+    order_status = serializers.CharField(default='PENDING')
+    quantity=serializers.IntegerField()
+    created_at=serializers.DateTimeField()
+    updated_at=serializers.DateTimeField()
+    
+    
+    class Meta:
+        model=Order
+        fields=['id', 'size', 'order_status', 'quantity', 'created_at', 'updated_at']
+        
+
 class OrderStatusUpdateSerializer(serializers.ModelSerializer):
     order_status = serializers.CharField(default='PENDING')
     
